3b28f8eef83ae46f01a206f0b35544b0e59467e6
Gabriel Fonseca Vassolli De Matos <gfmatos@rd.com.br>
2025-12-08 23:15:58 -0300

    [add] adiciona dependencias de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 plugins: [react()],
-server: {
-port: 5173,
-},
+base: "/Sentinela_projeto_EDEN/",
+// server: {
+// port: 5173,
+// },
 });
